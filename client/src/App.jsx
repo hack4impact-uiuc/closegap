d2d2f3b685c291845ec4a2dd00889dfc8f8401f1
Polarpi <eugeniachen123@gmail.com>
2020-09-28 21:08:24 -0500

    add right side of navbar + add to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HomeView from "./pages/HomeView";
+import Navbar from "./components/Navbar";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <HomeView />
     </div>
   );
--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
+import PermIdentityIcon from "@material-ui/icons/PermIdentityOutlined";
 import Toolbar from "@material-ui/core/Toolbar";
 import "./styles.css";
 
+const LoginLink = () => {
+  return (
+    <a id="nav-login" href="#login">
+      Login
+    </a>
+  );
+};
+
+const LogoutLink = () => {
+  return (
+    <a id="nav-logout" href="#logout">
+      Logout
+    </a>
+  );
+};
+
+const ProfileLink = () => {
+  const [name, setName] = useState("Person");
+  return (
+    <div id="nav-profile">
+      <PermIdentityIcon id="nav-profile-icon" />
+      <a id="nav-profile-link" href="#profile">
+        Hi {name}
+      </a>
+    </div>
+  );
+};
+
 const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  let rightNav;
+
+  if (isLoggedIn) {
+    rightNav = (
+      <div class="right-nav">
+        <ProfileLink />
+        <LogoutLink />
+      </div>
+    );
+  } else {
+    rightNav = <LoginLink />;
+  }
   return (
     <div>
       <AppBar>
         <Toolbar classes={{ root: "nav" }}>
           <img id="nav-logo" src="/imgs/logo-white.png" alt="Closegap Logo" />
+          {rightNav}
         </Toolbar>
       </AppBar>
     </div>
