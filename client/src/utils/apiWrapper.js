9318b2f3d510d53322eb18a23020561d5863ea96
Arman R <armanr123@gmail.com>
2020-09-29 22:42:02 -0700

    Add sample API Wrapper

--- client/src/utils/apiWrapper.js
+++ client/src/utils/apiWrapper.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/";
+const BASE_URL_TEST = "https://swapi.dev/api/"; // URL for Sample API
 
 export const getStudents = () => {
   /**
@@ -15,5 +16,32 @@ export const getStudents = () => {
         "Content-Type": "application/JSON",
       },
     })
-    .catch((error) => error({ type: "GET_STUDENTS_FAIL", error }));
+    .catch((error) =>
+      error({
+        type: "GET_STUDENTS_FAIL",
+        error,
+      })
+    );
+};
+
+// Sample API Wrapper
+export const getPeople = () => {
+  /**
+   * Returns all people
+   * Returns GET_PEOPLE_FAIL upon failure
+   */
+  const requestString = `${BASE_URL_TEST}people`;
+
+  return axios
+    .get(requestString, {
+      headers: {
+        "Content-Type": "application/JSON",
+      },
+    })
+    .catch((error) =>
+      error({
+        type: "GET_PEOPLE_FAIL",
+        error,
+      })
+    );
 };
