a0cbc74841f250a1a9471dafe96c549d4d940655
Jeffrey Tang <jt12@illinois.edu>
2020-09-30 14:48:15 -0500

    Add db module

---
+++ api/src/db.js
@@ -0,0 +1,12 @@
+const pgp = require("pg-promise")();
+
+// setup DB
+const db = pgp({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
+
+module.exports = db;
