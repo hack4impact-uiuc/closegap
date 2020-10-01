c02b450707d5b3ba864cdd931c5ee44e286c44f9
Arman R <armanr123@gmail.com>
2020-10-01 15:32:10 -0700

    Remove sample API request

--- client/src/utils/apiWrapper.js
+++ client/src/utils/apiWrapper.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/";
-const BASE_URL_TEST = "https://swapi.dev/api/"; // URL for Sample API
 
 export const getStudents = () => {
   /**
@@ -19,28 +18,6 @@ export const getStudents = () => {
     .catch((error) =>
       error({
         type: "GET_STUDENTS_FAIL",
-        error,
-      })
-    );
-};
-
-// Sample API Wrapper
-export const getPeople = () => {
-  /**
-   * Returns all people
-   * Returns GET_PEOPLE_FAIL upon failure
-   */
-  const requestString = `${BASE_URL_TEST}people`;
-
-  return axios
-    .get(requestString, {
-      headers: {
-        "Content-Type": "application/JSON",
-      },
-    })
-    .catch((error) =>
-      error({
-        type: "GET_PEOPLE_FAIL",
         error,
       })
     );
