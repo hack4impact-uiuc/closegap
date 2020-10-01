d59f041f6b78a1cb59980cf48c2fedcb81714d97
Jeffrey Tang <jt12@illinois.edu>
2020-10-01 00:46:43 -0500

    Run prettier

--- api/src/services/student.js
+++ api/src/services/student.js
@@ -1,6 +1,7 @@
 const db = require("../db");
 
-const getStudentById = (id) => db.oneOrNone("SELECT * FROM students WHERE id = $1", [id]);
+const getStudentById = (id) =>
+  db.oneOrNone("SELECT * FROM students WHERE id = $1", [id]);
 
 module.exports = {
   getStudentById,
