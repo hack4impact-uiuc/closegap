4632fc46cdbc8dfa44e0e7cdb189268c4d83ebf6
Polarpi <eugeniachen123@gmail.com>
2020-10-01 17:39:35 -0500

    Add index.js to /components folder to export default components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./pages/HomeView";
-import Navbar from "./components/Navbar";
+import { Navbar } from "./components";
 import "./styles/App.css";
 
 function App() {
