f9cb917808463f4b29c8a30cc309ef27862982cc
Polarpi <eugeniachen123@gmail.com>
2020-10-01 17:51:58 -0500

    Change class to className

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
   let loginOptions;
 
   loginOptions = isLoggedIn ? (
-    <div class="right-nav">
+    <div className="right-nav">
       <ProfileLink />
       <LogoutLink />
     </div>
