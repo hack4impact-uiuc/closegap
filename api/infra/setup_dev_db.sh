2f1e0b5beff5b677fa9ae5a5626a9c7e8aa9394c
Jeffrey Tang <jt12@illinois.edu>
2020-10-01 00:22:00 -0500

    Fix setup db script not seeding data

--- api/infra/setup_dev_db.sh
+++ api/infra/setup_dev_db.sh
@@ -1,12 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 ROOT=$(git rev-parse --show-toplevel)
 
 while true; do
     read -p "Seed database with initial data? [Y/n] " yn
     case $yn in
-        "" ) SUFFIX="_seeded"; break;;
-        [Yy]* ) SUFFIX="_seeded"; break;;
+        "" | [Yy]* ) SUFFIX="_seeded"; break;;
         [Nn]* ) SUFFIX=""; break;;
         * ) echo "Please answer y or n.";;
     esac
@@ -17,5 +16,5 @@ createuser -d closegap-api
 createdb -U closegap-api closegap-api_development
 createdb -U closegap-api closegap-api_test
 
-psql closegap-api_development < ${ROOT}/api/schema/db/development${SEEDED}.sql
-psql closegap-api_test < ${ROOT}/api/schema/db/test${SEEDED}.sql
+psql closegap-api_development < ${ROOT}/api/schema/db/development${SUFFIX}.sql
+psql closegap-api_test < ${ROOT}/api/schema/db/test${SUFFIX}.sql
