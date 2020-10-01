5b69eccc71ebfda997d5410904a4b14b5f109eea
Jeffrey Tang <jt12@illinois.edu>
2020-09-30 23:50:48 -0500

    Add example route for accessing db via service

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,27 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const { getStudentById } = require("../services/user.js");
+
 
 /* GET home page. */
 router.get("/", function (req, res) {
   res.send("Closegap!");
+});
+
+/* GET for testing db. */
+router.get("/students/:studentId", async (req, res) => {
+  let student;
+
+  try {
+    student = await getStudentById(parseInt(req.params.studentId));
+  } catch (e) {
+    return res.status(400).json({
+      error: "database error",
+    });
+  }
+
+  return res.json(200, student);
 });
 
 module.exports = router;
